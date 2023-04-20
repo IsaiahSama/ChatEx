6d787583c2561765eb99ec6c2f533a08590f3d45
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-04-20 17:21:37 -0400

    Will now render messages received from the server (via other clients)

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,5 +1,7 @@
 const socket = io("http://localhost:5000");
 
+const messagesDiv = document.getElementById("messagesDiv");
+
 socket.emit("handshake", "connecting to server");
 
 socket.on("connect", (...args) => {
@@ -7,7 +9,21 @@ socket.on("connect", (...args) => {
 });
 
 socket.on("message", (message) => {
-  console.log("Received a message: ", message);
+  const messageDiv = document.createElement("div");
+  const nameDiv = document.createElement("div");
+  const contentDiv = document.createElement("div");
+
+  messageDiv.className = "message";
+  nameDiv.className = "sender";
+  contentDiv.className = "messageContent";
+
+  nameDiv.innerText = "Client";
+  contentDiv.innerText = message;
+
+  messageDiv.appendChild(nameDiv);
+  messageDiv.appendChild(contentDiv);
+
+  messagesDiv.appendChild(messageDiv);
 });
 
 document.getElementById("sendMessage").onclick = () => {
